1ce8e691dbf36ef2b5ffbbadeaded28b1af553a1
Christian Veiby <chriveib@stud.ntnu.no>
2023-02-07 16:19:16 +0100

    feat: Authenticate users, create users and other
    Implement functionality for storing users in the database as well as
    authenticating users and storing a session.
    
    Refs: #18

---
+++ server/Services/userService.js
@@ -0,0 +1,18 @@
+const user = require("../Models/user");
+const userModel = require("../Models/user");
+
+exports.createUser = async (user) => {
+  return await userModel.create(user);
+};
+
+exports.getAllUsers = async () => {
+  return await userModel.find();
+};
+
+exports.getUserByName = async (username) => {
+  return await userModel.findOne({ username: username });
+};
+
+exports.updateUser = async (id, user) => {
+  return await userModel.findByIdAndUpdate(id, user);
+};
