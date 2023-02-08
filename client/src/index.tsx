aedad78206886c11bb9886ccc6875fc6c8a80c56
andrekda <andamme@yahoo.no>
2023-02-08 11:07:55 +0100

    Feat: created navbar

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RoutesView from "./navigation/RoutesView";
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
