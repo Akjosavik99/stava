3acf719d88688eb5b06fed494bdba24d7abea9a8
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-02-08 11:43:11 +0100

    feat: make log in page
    Made the login page with the same styling (ish) as the create user page.
    
    Refs: #16

--- client/src/navigation/RoutesView.tsx
+++ client/src/navigation/RoutesView.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import App from "../App";
-import LogIn from "../pages/LogIn";
+import LogInPage from "../pages/LogInPage";
 import SignUpPage from "../pages/SignUpPage";
 
 const RoutesView: React.FC = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LogIn />} />
+      <Route path="/login" element={<LogInPage />} />
       <Route path="/" element={<App />} />
       <Route path="/signup" element={<SignUpPage />} />
     </Routes>
