d9db3fd37654ae0f204358d256670fde50416443
Erik Menkin Lysfjord <erik.menkin@lysfjord.no>
2023-02-09 15:43:06 +0100

    feat: add functionality to add user
    The frontend is now connected to backend. Post request is sent with the
    necessary information.
    
    Refs: #17

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import RoutesView from "./navigation/RoutesView";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RoutesView />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <RoutesView />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
