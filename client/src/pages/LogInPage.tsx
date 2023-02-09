edab3dcd0dd849beaf929c5005299d4d0163df17
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-02-09 17:50:08 +0100

    feat: implement api-call to backend
    Trying to implement api calls to backend. Not finished.
    Refs: #23

--- client/src/pages/LogInPage.tsx
+++ client/src/pages/LogInPage.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+// import styled from "styled-components";
 import stava_logo from "../assets/stava_logo.svg";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 import {
   InputFieldLogInPage,
   Logo,
@@ -16,7 +18,34 @@ import {
   CreateUserButton,
 } from "../components/Form";
 
+type FormData = {
+  username: string;
+  password: string;
+};
+
+const useLoginMutation = () => {
+  const navigate = useNavigate();
+  return useMutation(
+    async (formData: FormData) => {
+      await axios.post("http://localhost:3001/api/user/auth", formData);
+    },
+    {
+      onSuccess: () => {
+        console.log("Sucess yay!");
+        navigate("/");
+      },
+    }
+  );
+};
+
 const LogInPage: React.FC = () => {
+  const [form, setForm] = useState({
+    username: "abc",
+    password: "abc",
+  });
+
+  const { mutate } = useLoginMutation();
+
   const navigate = useNavigate();
 
   return (
@@ -24,7 +53,8 @@ const LogInPage: React.FC = () => {
       <ContentContainer
         onSubmit={(e) => {
           e.preventDefault();
-          navigate("/");
+          mutate(form);
+          console.log("Sucess :D");
         }}
       >
         <LogoContainer>
@@ -33,10 +63,18 @@ const LogInPage: React.FC = () => {
         <Triangle></Triangle>
         <FormContainer>
           <InputContainer>
-            <InputFieldLogInPage placeholder="Username" name="username" />
+            <InputFieldLogInPage
+              placeholder="Username"
+              name="username"
+              // value={form.username}
+            />
           </InputContainer>
           <InputContainer>
-            <InputFieldLogInPage placeholder="Password" name="password" />
+            <InputFieldLogInPage
+              placeholder="Password"
+              name="password"
+              // value={form.password}
+            />
           </InputContainer>
           <InputContainer>
             <LogInButton id="logInButton">Log in</LogInButton>
--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -33,6 +33,12 @@ exports.createUser = async (req, res) => {
 };
 
 exports.loginUser = async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
   try {
     const user = await userService.getUserByName(req.body.username);
     if (user) {
