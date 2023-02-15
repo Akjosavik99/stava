9e79f561c9074fdc735c60ebd3bf36b342d64cfb
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2023-02-15 17:59:53 +0100

    test: create test for useMutation in logInPage
    You probably have to write "npm i --force" instead of "npm i" in
    stava/client from now on.
    
    Refs: #26

---
+++ client/src/tests/loginPage.test.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react";
+import { QueryClient } from "@tanstack/react-query";
+import { renderHook } from "@testing-library/react-hooks";
+import axios from "axios";
+import nock from "nock";
+import { act } from "@testing-library/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { useLoginMutation } from "../pages/LogInPage";
+
+jest.mock("axios");
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+// Disable console errors
+console.error = () => {};
+
+describe("Log in", () => {
+  test("useLoginMutation should navigate to '/' on success", async () => {
+    const { result, waitFor } = renderHook(() => useLoginMutation(), {
+      wrapper: wrapper,
+    });
+    nock("http://localhost:3001")
+      .post("/api/user/auth", {
+        username: "string",
+        password: "string",
+      })
+      .reply(200, {});
+    act(() => {
+      result.current.mutate({ username: "string", password: "string" });
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+      expect(mockedUsedNavigate).toBeCalledWith("/");
+    });
+  });
+});
