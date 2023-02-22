a6e1ddc0932b0261c9162889f7202bd44b475da0
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-02-22 09:36:18 +0100

    feat: make img show on exercise
    Made the images appear to the correct exercise now.
    Refs: #39

--- client/src/components/ExerciseComponent.tsx
+++ client/src/components/ExerciseComponent.tsx
@@ -5,18 +5,6 @@ import {
   RepsSetsContainer,
   RepsSets,
 } from "./Form";
-import bench from "../assets/workout_icons/bench.svg";
-import cycle from "../assets/workout_icons/cycle.svg";
-import dumbell from "../assets/dumbell.svg";
-import machine from "../assets/maskin.svg";
-import medicineBall from "../assets/medisinball.svg";
-import running from "../assets/running.svg";
-import yoga from "../assets/yoga.svg";
-import stretching from "../assets/stretching.svg";
-import pulldown from "../assets/pulldown.svg";
-import situps from "../assets/situps.svg";
-import hangups from "../assets/hangups.svg";
-import jumping from "../assets/jumping.svg";
 
 type Props = {
   img: string;
@@ -27,14 +15,10 @@ type Props = {
 const ExerciseComponent: React.FC<Props> = ({ img, reps, sets }: Props) => {
   return (
     <ExerciseContainer>
-      <ExerciseImage
-        src={`../assets/workout_icons/${img}.svg`}
-        alt={img}
-      ></ExerciseImage>
+      <ExerciseImage src={img} alt={img}></ExerciseImage>
       <RepsSetsContainer>
         <RepsSets>Reps: {reps}</RepsSets>
         <RepsSets>Sets: {sets}</RepsSets>
-        <RepsSets>Sets: {img}</RepsSets>
       </RepsSetsContainer>
     </ExerciseContainer>
   );
--- client/src/pages/ViewExercises.tsx
+++ client/src/pages/ViewExercises.tsx
@@ -1,30 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import ExerciseComponent from "../components/ExerciseComponent";
 import {
   DataContainer,
-  ExerciseContainer,
-  ExerciseImage,
   InnerExercisesContainer,
   OuterExercisesContainer,
-  RepsSets,
-  RepsSetsContainer,
 } from "../components/Form";
 import bench from "../assets/workout_icons/bench.svg";
 import cycle from "../assets/workout_icons/cycle.svg";
-import dumbell from "../assets/dumbell.svg";
-import machine from "../assets/maskin.svg";
-import medicineBall from "../assets/medisinball.svg";
-import running from "../assets/running.svg";
-import yoga from "../assets/yoga.svg";
-import stretching from "../assets/stretching.svg";
-import pulldown from "../assets/pulldown.svg";
-import situps from "../assets/situps.svg";
-import hangups from "../assets/hangups.svg";
-import jumping from "../assets/jumping.svg";
+import dumbell from "../assets/workout_icons/cycle.svg";
+import machine from "../assets/workout_icons/machine.svg";
+import medicineBall from "../assets/workout_icons/medicineBall.svg";
+import running from "../assets/workout_icons/running.svg";
+import yoga from "../assets/workout_icons/yoga.svg";
+import stretching from "../assets/workout_icons/stretching.svg";
+import pulldown from "../assets/workout_icons/pulldown.svg";
+import situps from "../assets/workout_icons/situps.svg";
+import hangups from "../assets/workout_icons/hangups.svg";
+import jumping from "../assets/workout_icons/jumping.svg";
 
 import axios from "axios";
-import { Exercise, Exercises } from "../util/types";
+import { Exercises } from "../util/types";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../components/Loading";
@@ -39,6 +35,37 @@ const useGetDataQuery = (id: string | undefined) => {
       });
   });
 };
+
+function updateImage(img: string) {
+  switch (img) {
+    case "bench":
+      return bench;
+    case "cycle":
+      return cycle;
+    case "dumbell":
+      return dumbell;
+    case "machine":
+      return machine;
+    case "medicineBall":
+      return medicineBall;
+    case "running":
+      return running;
+    case "yoga":
+      return yoga;
+    case "stretching":
+      return stretching;
+    case "pulldown":
+      return pulldown;
+    case "situps":
+      return situps;
+    case "hangups":
+      return hangups;
+    case "jumping":
+      return jumping;
+    default:
+      return bench;
+  }
+}
 
 const ViewExercises: React.FC = () => {
   const [exercises, setExercises] = React.useState<Exercises>({} as Exercises);
@@ -66,7 +93,7 @@ const ViewExercises: React.FC = () => {
               }) => {
                 return (
                   <ExerciseComponent
-                    img={exercise.exerciseName}
+                    img={updateImage(exercise.exerciseName)}
                     reps={exercise.reps}
                     sets={exercise.sets}
                   />
